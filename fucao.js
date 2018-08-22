9b1a8d00532a9ee377324423d5d0263480a48c25
laissayuri <k.lais.sayuri@gmail.com>
2018-08-22 12:01:27 -0300

    pronto

---
+++ fucao.js
@@ -0,0 +1,23 @@
+ let lista =[];
+ function procurar (lista){ 
+    for( numero of lista ){
+        if (numero===3){
+            return true;
+        }
+    }   
+return false;
+ }
+procurar(lista)
+
+//let lista = [3,4,2,5,7,1]
+//
+//function encontrarTres(lista) {
+  //  for(item of lista) {
+    //    if(item===3) {
+      //      return true;
+        //}
+    //}
+  //  return false;
+//}
+
+//encontrarTres(lista);
